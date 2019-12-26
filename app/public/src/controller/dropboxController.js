e940384fae22689cf57bb62bee0f967760f95f2d
lucas beat <clashbruno10@gmail.com>
2019-12-25 23:41:09 -0300

    adicionando controller

---
+++ app/public/src/controller/dropboxController.js
@@ -0,0 +1,13 @@
+class DropboxController {
+    constructor() {
+        this.btnSendFileEl = document.querySelector('#btn-send-file')
+        this.initEvents()
+    }
+
+    initEvents() {
+        this.btnSendFileEl.addEventListener('click', event => {
+
+        })
+    }
+
+}
