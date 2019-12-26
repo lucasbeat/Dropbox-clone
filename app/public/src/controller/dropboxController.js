76f76c3799132b56d56136d394802572fd1a0dd7
lucas beat <clashbruno10@gmail.com>
2019-12-26 03:21:47 -0300

    adicionado animação ao upload

--- app/public/src/controller/dropboxController.js
+++ app/public/src/controller/dropboxController.js
@@ -3,6 +3,9 @@ class DropboxController {
         this.btnSendFileEl = document.querySelector('#btn-send-file')
         this.inputFilesEl = document.querySelector('#files')
         this.snackModalEl = document.querySelector('#react-snackbar-root')
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
+        this.nameFileEl = this.snackModalEl.querySelector('.filename')
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft')
 
 
         this.initEvents()
@@ -14,10 +17,17 @@ class DropboxController {
         })
 
         this.inputFilesEl.addEventListener('change', event => {
+
             this.uploadTask(event.target.files)
-            this.snackModalEl.style.display = 'block'
+
+            this.modalShow()
+
+            this.inputFilesEl.value = '';
         })
 
+    }
+    modalShow(show = true) {
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
     }
 
     uploadTask(files) {
@@ -31,6 +41,8 @@ class DropboxController {
                 ajax.open('POST', '/upload');
 
                 ajax.onload = event => {
+
+                    this.modalShow(false)
                     try {
                         resolve(JSON.parse(ajax.responseText));
                     } catch (e) {
@@ -38,11 +50,18 @@ class DropboxController {
                     }
                 }
                 ajax.onerror = event => {
+                    this.modalShow(false)
                     reject(event);
                 };
 
+                ajax.upload.onprogress = event => {
+                    this.uploadProgress(event, file)
+                }
+
                 let formData = new FormData()
                 formData.append('input-file', file)
+
+                this.startUploadTime = Date.now()
 
                 ajax.send(formData);
 
@@ -50,5 +69,40 @@ class DropboxController {
         })
 
         return Promise.all(promises)
+    }
+
+
+    uploadProgress(event, file) {
+
+        let timespent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt((loaded / total) * 100);
+        let timeleft = ((100 - porcent) * timespent) / porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`
+
+        this.nameFileEl.innerHTML = file.name;
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeleft)
+
+
+    }
+
+    formatTimeToHuman(duration) {
+
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+            return `${hours} horas, ${minutes} minutos e ${seconds}segundos`
+        }
+        if (minutes > 0) {
+            return `${minutes} minutos e ${seconds}segundos`
+        }
+        if (seconds > 0) {
+            return `${seconds}segundos`
+        }
+        return '';
     }
 }
