9cfc8645818dcc62dd1a5c2109cde45fb773ef36
lucas beat <clashbruno10@gmail.com>
2020-01-01 21:59:23 -0300

    adicionando selecao de items

--- app/public/src/controller/dropboxController.js
+++ app/public/src/controller/dropboxController.js
@@ -348,7 +348,38 @@ class DropboxController {
 
     initEventsLi(li) {
         li.addEventListener('click', e => {
-            li.classList.toggle('selected')
+
+            if (!e.shiftKey) {
+                let firstLi = this.listFilesEl.querySelector('li.selected')
+
+                if (firstLi) {
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                    lis.forEach((el, index) => {
+                        if (firstLi === el) indexStart = index
+                        if (li === el) indexEnd = index
+                    })
+
+                    let index = [indexStart, indexEnd].sort()
+
+                    lis.forEach((el, i) => {
+                        if (i >= index[0] && i <= index[1]) {
+                            el.classList.add('selected')
+                        }
+                    })
+                    return true;
+                }
+            }
+
+            if (!e.ctrlKey) {
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+                    el.classList.remove('selected')
+                })
+            }
+
+            li.classList.toggle('selected');
         })
     }
 
